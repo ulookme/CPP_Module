d13b364cf5fc3c63e4cfedb00abaec5046f637f4
Chajjar <48330531+ulookme@users.noreply.github.com>
2023-01-13 12:34:24 +0100

    Add files via upload

--- CPP_Module03/ex00/ClapTrap.cpp
+++ CPP_Module03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: chajjar <chajjar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:19:51 by chajjar           #+#    #+#             */
-/*   Updated: 2023/01/12 23:19:23 by chajjar          ###   ########.fr       */
+/*   Updated: 2023/01/13 12:33:12 by chajjar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 ClapTrap::ClapTrap()
 {
-
+	//std::cout << "Default constructor called "<< std::endl;
 }
 
 ClapTrap::ClapTrap(const std::string &name){
 
+	std::cout << " String constructor called "<< std::endl;
 	_name = name;
 	_hit = 10;
 	_energy = 10;
@@ -27,6 +28,7 @@ ClapTrap::ClapTrap(const std::string &name){
 
 ClapTrap::ClapTrap(const ClapTrap& src)
 {
+	std::cout << "Copie constructor called "<< std::endl;
 	_name = src._name;
 	_hit = src._hit;
 	_energy = src._energy;
@@ -35,23 +37,55 @@ ClapTrap::ClapTrap(const ClapTrap& src)
 
 ClapTrap::~ClapTrap()
 {
-
+	std::cout << "Destructor called "<< std::endl;
 }
 
 void ClapTrap::attack(const std::string& target){
-
+	if(_hit <= 0)
+	{
+		std::cout << "il dort pour longtemps ;( Bonne chance pour le reveiller  pas d'attack"<< std::endl;
+		return ;
+	}
+	if(_energy <=0)
+	{
+		std::cout << "pas d'ernergie donc pas d'attak ;( "<< std::endl;
+		return ;
+	}
+	_energy -= 1;
+	std::cout << "ClapTrap " << _name << " attacks " << target << " ,causing "<< _attack <<" points of damage!" << std::endl;
+	
 }
 
 void ClapTrap::takeDamage(unsigned int amount){
-	
+	if(_hit <= 0)
+	{
+		std::cout << "il dort pour longtemps ;( Bonne chance pour le reveiller pas de dommage "<< std::endl;
+		return ;
+	}
+	_hit -= amount;
+	std::cout << "ClapTrap " << this->_hit << " hit : points of life!" << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount){
-	
+	if(_hit <= 0)
+	{
+		std::cout << "il dort pour longtemps ;( Bonne chance pour le reveiller pas de repaire :) "<< std::endl;
+		return ;
+	}
+	if(_energy <= 0)
+	{
+		std::cout << "pas d'ernergie donc pas de reparation ;) "<< std::endl;
+		return ;
+	}
+	_energy -= 1;
+	_hit += amount;
+	std::cout << "ClapTrap  Repare  life ++" << amount << "!" << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap &src){
+	std::cout << " assignment operator called "<< std::endl;
 	_hit = src._hit;
 	_energy = src._energy;
 	_attack = src._attack;
+	return(*this);
 }
--- CPP_Module03/ex00/main.cpp
+++ CPP_Module03/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: chajjar <chajjar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 20:20:03 by chajjar           #+#    #+#             */
-/*   Updated: 2023/01/12 20:20:04 by chajjar          ###   ########.fr       */
+/*   Updated: 2023/01/13 12:31:24 by chajjar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main(){
+	ClapTrap a("testeur00");
+	ClapTrap b("testeur01");
+	std::cout << "======================================="<< std::endl;
+	a.takeDamage(100);
+	std::cout << "======================================="<< std::endl;
+	a.attack("testeur03");
+	std::cout << "======================================="<< std::endl;
+	a.beRepaired(5);
+	std::cout << "======================================="<< std::endl;
+	return(0);
+}
