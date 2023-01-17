24230ac4575b0885a3ef217aad9ff902d3a675b4
Chajjar <48330531+ulookme@users.noreply.github.com>
2023-01-17 17:09:17 +0100

    ex01 done

--- CPP_Module06/ex01/Data.hpp
+++ CPP_Module06/ex01/Data.hpp
@@ -6,7 +6,18 @@
 /*   By: chajjar <chajjar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 16:45:26 by chajjar           #+#    #+#             */
-/*   Updated: 2023/01/17 16:45:26 by chajjar          ###   ########.fr       */
+/*   Updated: 2023/01/17 16:50:33 by chajjar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+
+struct Data{
+	int val;
+};
+
+uintptr_t serialize(Data* ptr);
+Data* deserialize(uintptr_t raw);
 
--- CPP_Module06/ex01/main.cpp
+++ CPP_Module06/ex01/main.cpp
@@ -6,7 +6,28 @@
 /*   By: chajjar <chajjar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 16:45:29 by chajjar           #+#    #+#             */
-/*   Updated: 2023/01/17 16:45:30 by chajjar          ###   ########.fr       */
+/*   Updated: 2023/01/17 17:08:18 by chajjar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Data.hpp"
+
+uintptr_t serialize(Data *ptr)
+{
+	return(reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return(reinterpret_cast<Data*>(raw));
+}
+
+int main(){
+	Data* data;
+	uintptr_t raw;
+	Data* val_retour;
+	raw = serialize(data);
+	std::cout << " serialize : " << raw << std::endl;
+	val_retour = deserialize(raw);
+	std::cout << " deserialize : " << val_retour << std::endl;
+}
