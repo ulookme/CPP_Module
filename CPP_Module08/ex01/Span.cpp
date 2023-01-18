acf89cce30e233ee719f8d4ad9ca5d67eb757a63
Chajjar <48330531+ulookme@users.noreply.github.com>
2023-01-18 21:08:49 +0100

    Add files via upload

--- CPP_Module08/ex01/Span.cpp
+++ CPP_Module08/ex01/Span.cpp
@@ -6,7 +6,51 @@
 /*   By: chajjar <chajjar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 19:20:57 by chajjar           #+#    #+#             */
-/*   Updated: 2023/01/18 19:20:58 by chajjar          ###   ########.fr       */
+/*   Updated: 2023/01/18 21:08:12 by chajjar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span(int n):_N(n){
+	_array.resize(n);
+}
+
+Span::~Span()
+{
+}
+
+Span::Span(const Span &t):_N(t._N), _array(t._array)
+{
+
+}
+
+Span& Span::operator=(const Span &t){
+	this->_array = t._array;
+	this->_N = t._N;
+	return(*this);
+}
+
+void Span::addNumber(const int &n){
+	if(this->_array.size() >= _N)
+		throw SpanNoSpaceExceptin();
+	this->_array.push_back(n);
+}
+
+size_t Span::shortestSpan(){
+	
+}
+
+size_t Span::longestSpan(){
+	if(this->_array.size() <= 1)
+		throw IsEmpty();
+	std::sort(_array.begin(), _array.end());
+	return(*(_array.end() - 1) - *_array.begin());
+}
+
+const char* Span::SpanNoSpaceExceptin::what() const throw(){
+	return("Span No Space Exception");
+}
+const char* Span::IsEmpty::what() const throw(){
+	return(" Is Empty Exception");
+}
--- CPP_Module08/ex01/Span.hpp
+++ CPP_Module08/ex01/Span.hpp
@@ -6,7 +6,36 @@
 /*   By: chajjar <chajjar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 19:21:00 by chajjar           #+#    #+#             */
-/*   Updated: 2023/01/18 19:21:01 by chajjar          ###   ########.fr       */
+/*   Updated: 2023/01/18 20:38:51 by chajjar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <exception>
+
+class Span
+{
+private:
+	int _N;
+	std::vector<int> _array;
+	Span();
+public:
+	Span(int n);
+	Span(const Span &t);
+	Span& operator=(const Span &t);
+	~Span();
+	void addNumber(const int &n);
+	size_t shortestSpan();
+	size_t longestSpan();
+	class  SpanNoSpaceExceptin: public std::exception{
+		const char *what() const throw();
+	};
+	class  IsEmpty: public std::exception{
+		const char *what() const throw();
+	};
+};
 
